7bb57439bfe74641bfee00c9d1ecf2f34cf040ec
Dillon Hammond <dillonhammond@gmail.com>
2016-07-20 19:45:36 -0500

    Removed println

--- src/lexer.rs
+++ src/lexer.rs
@@ -111,7 +111,6 @@ impl Lexer {
         }
         self.input = strip_input;
         self.previous_token = Some(token.clone());
-        println!("{:?}", token);
         Ok(token)
     }
     pub fn revert(&mut self) {
